8c93e9c925a518cdaaab47427f2acc4c5eb71720
Paola Schiaffino <paola_schiaffino@yahoo.co.uk>
2018-06-24 16:19:19 +0100

    Refactored tests: now they use the .create method to insert data into the database

--- spec/chitter_spec.rb
+++ spec/chitter_spec.rb
@@ -2,11 +2,9 @@ require 'chitter'
 describe Chitter do
   describe '.all' do
     it 'returns all the posted messages' do
-      connection = PG.connect(dbname: 'chitter_manager_test')
-      connection.exec("INSERT INTO chitter VALUES(1, 'hola a todos');")
-      connection.exec("INSERT INTO chitter VALUES(2, 'que pasaaaa');")
-      expected_messages = ['hola a todos', 'que pasaaaa']
-      expect(Chitter.all).to eq expected_messages
+      Chitter.create(message: 'hola a todos')
+      Chitter.create(message: 'que pasaaaa')
+      expect(Chitter.all).to eq ['hola a todos', 'que pasaaaa']
     end
   end
   describe '.create' do
--- spec/features/viewing_messages_spec.rb
+++ spec/features/viewing_messages_spec.rb
@@ -1,9 +1,7 @@
 feature 'viewing messages' do
   scenario 'the user sees the list of posted messages in the homepage' do
-    connection = PG.connect(dbname: "chitter_manager_test")
-    connection.exec("INSERT INTO chitter VALUES(1, 'hola a todos');")
-    connection.exec("INSERT INTO chitter VALUES(2, 'que pasaaaa');")
-
+    Chitter.create(message: 'hola a todos')
+    Chitter.create(message: 'que pasaaaa')
     visit('/')
     expect(page).to have_content 'hola a todos'
     expect(page).to have_content 'que pasaaaa'
